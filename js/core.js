b7e64ad734853d076a947a7d5a243e084b3a3f04
Henok Mikre <henok@blencorp.com>
2018-02-17 17:26:35 -0500

    Hid deprecated logic.

--- js/core.js
+++ js/core.js
@@ -2,7 +2,7 @@
   $(document).ready(function() {
     // Show upcoming events by default.
     // @see https://getbootstrap.com/docs/3.3/javascript/#tabs.
-    $('#eventsTab a[href="#upcoming"]').tab('show') // Select tab by name
+    //$('#eventsTab a[href="#upcoming"]').tab('show') // Select tab by name
   });
 })(jQuery);
 
