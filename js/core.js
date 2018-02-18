29ce9809b60fa888e0306fdc23c41fd537f35771
Henok Mikre <henok@blencorp.com>
2018-02-18 12:15:03 -0500

    Removed debugging code.

--- js/core.js
+++ js/core.js
@@ -2,10 +2,10 @@
   $(document).ready(function() {
     // Show upcoming events by default.
     // @see https://getbootstrap.com/docs/3.3/javascript/#tabs.
+    // @see https://stackoverflow.com/a/40142438/871793.
     //$('#eventsTab a[href="#upcoming"]').tab('show') // Select tab by name
 
     // Events landing page.
-    //$('body.events .nav-tabs > li:first > a').append(' (1)');
     if ($("body").hasClass("events")) {
       len1 = $('.tab-content #menu1 .event-item').length;
       len2 = $('.tab-content #menu2 .event-item').length;
