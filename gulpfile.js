90d1227a02827d09d2be34769fc8b5097c7b655f
Henok Mikre <henok@blencorp.com>
2018-02-18 19:20:31 -0500

    Added bundle exec to fix issues.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var browserSync = require('browser-sync').create();
 // Task for building blog when something changed:
 //gulp.task('build', shell.task(['bundle exec jekyll build --watch --drafts --baseurl ""']));
 // Or if you don't use bundle:
-gulp.task('build', shell.task(['jekyll build --watch --baseurl ""']));
+gulp.task('build', shell.task(['bundle exec jekyll build --watch --baseurl ""']));
 
 // Task for serving blog with Browsersync
 gulp.task('serve', function () {
